63fed8233bc39aa6bc59a016836a1beadef17c85
Mikhail Perminov <mperminov.work@gmail.com>
2026-05-04 23:20:20 +0300

    core: handle empty proxy selector result

--- core/src/test/java/io/grpc/internal/ProxyDetectorImplTest.java
+++ core/src/test/java/io/grpc/internal/ProxyDetectorImplTest.java
@@ -40,6 +40,8 @@ import java.net.Proxy;
 import java.net.ProxySelector;
 import java.net.SocketAddress;
 import java.net.URI;
+import java.util.Collections;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -189,6 +191,13 @@ public class ProxyDetectorImplTest {
           }
         },
         authenticator);
+    assertNull(proxyDetector.proxyFor(destination));
+  }
+
+  @Test
+  public void returnNullWhenProxySelectorReturnsEmptyList() throws Exception {
+    when(proxySelector.select(any(URI.class))).thenReturn(Collections.<Proxy>emptyList());
+
     assertNull(proxyDetector.proxyFor(destination));
   }
 }
